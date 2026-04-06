3652c520d2677462463fdc79b74ba128b1d9bf42
Kandwana Honathan <nathankandwanaho3@gmail.com>
2026-04-06 13:41:24 +0200

    spare_parts

--- frontend/my-app/src/pages/admin-dash.jsx
+++ frontend/my-app/src/pages/admin-dash.jsx
@@ -5,13 +5,16 @@ export default function Admin() {
     if (!user) {
         return <h2>first login</h2>
     }
+    const [active, setActive]=useState("dashboard")
     return(
         <div className="flex">
 
             <div className="w-64 h-screen bg-gray-800 text-white p-4">
                 <div className="bg-white rounded p-2 "><h2 className="font-bold text-xl text-blue-800 rounded">Nick Spares</h2></div>
                 <ul className="space-y-3">
-                    <li className=" text-xl hover:bg-blue-800 p-3 rounded" >Dashboard</li>
+                    <li className={`p-2 rounded cursor-pointer ${
+                        active==="dashboard"? "bg-gray-500":"hover:bg-blue-800"
+                    }`} >Dashboard</li>
                     <li className=" text-xl hover:bg-blue-800 p-3 rounded">Products</li>
                     <li className=" text-xl hover:bg-blue-800 p-3 rounded">Categories</li>
                     <li className=" text-xl hover:bg-blue-800 p-3 rounded">Stock</li>
--- frontend/my-app/src/pages/dashboard.jsx
+++ frontend/my-app/src/pages/dashboard.jsx
@@ -3,22 +3,22 @@
 export default function DashboardHome () {
     return(
         <div>
-            <div className="grid grid-cols-4 p-6  text-white">
-                <div className="w-70 shadow-lg bg-blue-500 rounded">
-                    <h3>Total-product</h3>
-                    <p>128</p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  gap-6 p-4 text-white">
+                <div className="w-70 shadow-lg bg-blue-500 h-32 rounded">
+                    Total-product
+                    <p className="text-bold text-2xl ">128</p>
                 </div>
-                <div className="w-70 shadow-lg bg-blue-500 rounded">
-                    <h3>Categories</h3>
-                    <p>128</p>
+                <div className="w-70 shadow-lg h-32 bg-blue-500 rounded">
+                    <h3 >Categories</h3>
+                    <p className="text-bold text-2xl">128</p>
                 </div>
-                <div className="w-70 shadow-lg bg-blue-500 rounded">
+                <div className="w-70 shadow-lg h-32 bg-blue-500 rounded">
                     <h3>low-stock</h3>
-                    <p>5</p>
+                    <p className="text-bold text-2xl">5</p>
                 </div>
-                <div className="w-70 shadow-lg bg-blue-500 rounded">
+                <div className="w-70 shadow-lg h-32 bg-blue-500 rounded">
                     <h3>Registered_User</h3>
-                    <p>2</p>
+                    <p className="text-bold text-2xl">2</p>
                 </div>
             </div>
         </div>
